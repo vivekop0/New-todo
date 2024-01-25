339f3e18353222382669b0d6bd03c0f685758b82
vivekop0 <sarkarjii534@gamil.com>
2024-01-25 13:29:38 +0530

    added new stuff

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-let inp = document.getElementById('title')
-let inp2 = document.getElementById('description')
-let btn = document.querySelector(".add")
-let container = document.querySelector(".conatainer")
+let btn = document.querySelector(".add");
 
+let container = document.querySelector(".conatainer");
 
-btn.addEventListener('click',()=>{
-    let childDiv = document.createElement('div')
-    let divTitle = document.createElement('div')
-    let divDesc =document.createElement("div")
-    let btn2 = document.createElement("button")
-    btn2.innerText = "delete"
-    container.appendChild(childDiv)
-    childDiv.appendChild(divTitle).innerHTML = inp.value
-childDiv.appendChild(divDesc).innerHTML =inp2.value
-childDiv.appendChild(btn2)
-})
+let globalid =1;
+function createChild(title,description,id){
+    let childDiv = document.createElement("div");
+    let divTitle = document.createElement("div");
+    let divDesc = document.createElement("div");
+    let btn2 = document.createElement("button");
+    btn2.innerText = "mark as done";
+    container.appendChild(childDiv);
+    childDiv.appendChild(divTitle).innerHTML = title.value;
+    childDiv.appendChild(divDesc).innerHTML = description.value;
+    childDiv.appendChild(btn2);
+    childDiv.setAttribute("id",id)
+    btn2.addEventListener('click',()=>{
+      btn2.innerText = "done"
+    })
+    return childDiv;
+}
+btn.addEventListener("click", () => {
+    let inp = document.getElementById("title");
+let inp2 = document.getElementById("description");
 
+container.appendChild(createChild(inp,inp2,globalid++))
+
+});
